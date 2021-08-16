4499e427d5e16e0deb69635f3b126a5d9ede5a3e
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-08-17 01:17:23 +0300

    create start.sh file

--- config.py
+++ config.py
@@ -3,6 +3,15 @@ class Config:
     '''
     General configuration parent class
     '''
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SECRET_KEY = 'estherkey'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 class ProdConfig(Config):
     '''
     Production  configuration child class
