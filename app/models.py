05b10d11e0cd8087797d268d1541c03f1cfe7474
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-08-16 23:42:33 +0300

    add test user file

--- app/models.py
+++ app/models.py
@@ -9,14 +9,15 @@ class User(db.Model):
 
     @property
     def password(self):
-        raise AttributeError('You cannot read the password attrib
-                             @ password.setter
+        raise AttributeError('You cannot read the password attribute')
 
+    @password.setter
     def password(self, password):
         self.pass_secure = generate_password_hash(password)
 
     def verify_password(self, password):
         return check_password_hash(self.pass_secure, password)
+
 
 
     def __repr__(self):
