949b18cf24d5d717a66b781919145812139d67ef
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-08-16 23:37:10 +0300

    add property decorator with verfy password method to take passowds

--- app/models.py
+++ app/models.py
@@ -1,11 +1,22 @@
+from werkzeug.security import generate_password_hash,check_password_hash
 from . import db
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attrib
+                             @ password.setter
 
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
 
     def __repr__(self):
