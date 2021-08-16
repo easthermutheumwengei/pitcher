92af38e5f304e1d62d320b10cea44fd8671bb044
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-08-16 15:32:57 +0300

    add manager shell user

--- manage.py
+++ manage.py
@@ -10,5 +10,12 @@ manager = Manager(app)
 migrate = Migrate(app,db)
 
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
+
+
+
+
 if__name__ == '__main__':
     manager.run()
