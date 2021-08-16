19ec28aacf2180a6ab5fa9c0d4332af84a9474ed
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-08-16 22:56:07 +0300

    add requirements.txt

--- app/models.py
+++ app/models.py
@@ -5,5 +5,8 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
 
+
+
+
     def __repr__(self):
         return f'User {self.username}'
